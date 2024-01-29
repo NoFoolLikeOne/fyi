2fb0cec45d561ef9f334d2dc5443006f84663c2e
NoFoolLikeOne <31373535+NoFoolLikeOne@users.noreply.github.com>
2024-01-29 21:48:01 +0000

    comment

--- redirect.js
+++ redirect.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const path = window.location.pathname.substring(1);
 
   // Make a request to your API using the path
-  const apiUrl = "https://us-central1-canonn-api-236217.cloudfunctions.net/query/fyi/" + path;
+  //const apiUrl = "https://us-central1-canonn-api-236217.cloudfunctions.net/query/fyi/" + path;
   const apiUrl = "http://192.168.0.72:8080/fyi/" + path;
 
   fetch(apiUrl)
